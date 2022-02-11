a8442c57e437e6fa10afd1f1814d1c739b035f81
Macéo <maceovalente@gmail.com>
2022-02-11 16:41:55 +0100

    Writing function.py code

---
+++ projet/function.py
@@ -0,0 +1,18 @@
+print("Hello world !")
+
+def addition(x,y):
+    """
+    computing the sum of x and y
+    Args :
+        x : (float)
+        y : (float)
+    Returns :
+        float : sum
+    >>> addition(3,4)
+    7
+    >>> addition(2.3,7.1)
+    9.4
+    """
+    return(x+y)
+
+print(addition(6.7,8.9))
