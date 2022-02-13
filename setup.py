03e6ae14c8fdb1d63e344c7fcc9ff4aae617ea05
Macéo <maceovalente@gmail.com>
2022-02-13 20:20:10 +0100

    setup.py finished, ready to upload package on PyPI

--- setup.py
+++ setup.py
@@ -3,4 +3,19 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup
+setuptools.setup(
+    name="packageGrp5",
+    version = "0.0.1",
+    author = "Group 5",
+    author_email = "maceovalente@gmail.com",
+    description = "4BIM project by group 5",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lina-ouaret/Project_4bim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independant"
+    ],
+)
