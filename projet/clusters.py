25c0881584719c6eb0b705d8d9b35f2ab29a06e7
CharlotteLoiselDecesvre <charlotte.loisel@insa-lyon.fr>
2022-03-31 17:58:10 +0200

    Ajout de l'upload des photos du cluster1 dans fichier clusters.py

--- projet/clusters.py
+++ projet/clusters.py
@@ -2,9 +2,16 @@ import numpy as np
 import pandas as pd
 import random
 import common_functions as cf
+import sys
+from matplotlib import image
+from matplotlib import pyplot
+from PIL.Image import *
+import glob
+from skimage import io, transform
+import skimage
 
 
-att_cluster1 = {"Male":-1,"Straight_Hair":-1,"Young":-1} 
+att_cluster1 = {"Male":-1,"Straight_Hair":-1,"Young":-1}
 att_cluster2= {"Male":-1,"Straight_Hair":-1,"Young":1}
 att_cluster3 = {"Male":-1,"Straight_Hair":1,"Young":1}
 att_cluster4 = {"Male":1,"Straight_Hair":1,"Young":-1}
@@ -24,3 +31,20 @@ index_cluster5 =cf.matrix_reduction(df_attributes,att_cluster5)
 index_cluster6 =cf.matrix_reduction(df_attributes,att_cluster6)
 index_cluster7 =cf.matrix_reduction(df_attributes,att_cluster7)
 index_cluster8 =cf.matrix_reduction(df_attributes,att_cluster8)
+
+
+#Upload pictures avec ckuster1
+for k in range(len(index_cluster1)):
+    images.append(glob.glob("img_align_celeba/"+index_cluster1[k]+".jpg")[0])
+i=0
+n=len(images)
+img=[None]*n
+image_size=(128,128)
+for image_ in images:
+    picture = pyplot.imread(image_)
+    img[i] = transform.resize(picture, image_size)
+    i+=1
+dataset=[None]*n
+for j in range(n):
+    dataset[j]=np.reshape(np.asarray(img[j]),(128*128*3))
+data=np.array(dataset)
